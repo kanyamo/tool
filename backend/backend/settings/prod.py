b8abf59fb01394bd93ba739bce15f2fbc9ef6a7e
kanyamo <kanajir@yahoo.co.jp>
2022-12-28 21:25:43 +0900

    prodのロギング設定を変更

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -2,3 +2,48 @@ from .base import *
 from .utils import str_to_bool
 
 DEBUG = str_to_bool(os.getenv("DEBUG", "y"))
+
+# 本番環境のロギング設定
+LOGGING = {
+    'version': 1,  # 1固定
+    'disable_existing_loggers': False,
+
+    # ロガーの設定
+    'loggers': {
+        # Djangoが利用するロガー
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+        },
+        # diaryアプリケーションが利用するロガー
+        'core': {
+            'handlers': ['file'],
+            'level': 'INFO',
+        },
+    },
+
+    # ハンドラの設定
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/django.log'),
+            'formatter': 'prod',
+            'when': 'D',  # ログローテーション(新しいファイルへの切り替え)間隔の単位(D=日)
+            'interval': 1,  # ログローテーション間隔(1日単位)
+            'backupCount': 7,  # 保存しておくログファイル数
+        },
+    },
+
+    # フォーマッタの設定
+    'formatters': {
+        'prod': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        },
+    }
+}
